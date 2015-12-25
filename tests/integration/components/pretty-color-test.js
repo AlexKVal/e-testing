7760f4c8afb3c194e5462b6465e556dcb65ffa90
AlexKVal <alexkval@gmail.com>
2015-12-25 23:09:04 +0200

    [added] 'pretty-color' component with integration test

---
+++ tests/integration/components/pretty-color-test.js
@@ -0,0 +1,21 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('pretty-color', 'Integration | Component | pretty color', {
+  integration: true
+});
+
+test('should change colors', function(assert) {
+  assert.expect(2);
+
+  // set the outer context to red
+  this.set('colorValue', 'red');
+
+  this.render(hbs`{{pretty-color name=colorValue}}`);
+
+  assert.equal(this.$('div').attr('style'), 'color: red', 'starts as red');
+
+  this.set('colorValue', 'blue');
+
+  assert.equal(this.$('div').attr('style'), 'color: blue', 'updates to blue');
+});
