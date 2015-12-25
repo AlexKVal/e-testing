d0a69bb8f31d82b9818cd3ceab082ce1b0ddcfe5
AlexKVal <alexkval@gmail.com>
2015-12-25 23:32:03 +0200

    [changed] rewrite test with Ember@2 syntax

--- tests/integration/components/comment-form-test.js
+++ tests/integration/components/comment-form-test.js
@@ -8,13 +8,13 @@ moduleForComponent('comment-form', 'Integration | Component | comment form', {
 test('should trigger external action on form submit', function(assert) {
   assert.expect(1);
 
-  // test double for the external action
-  this.set('externalAction', (actual) => {
+  // external action
+  this.on('externalAction', function(actual) {
     const expected = { comment: 'You are not a wizard!' };
     assert.deepEqual(actual, expected, 'submitted value is passed to external action');
   });
 
-  this.render(hbs`{{comment-form submitComment=(action externalAction)}}`);
+  this.render(hbs`{{comment-form submitComment=(action 'externalAction')}}`);
 
   // fill out the form and force an onchange
   this.$('textarea').val('You are not a wizard!');
