83f87cdcffc0466604b14e3c3ea4a696cfa48301
AlexKVal <alexkval@gmail.com>
2015-12-27 23:36:28 +0200

    [added] testing model's methods

---
+++ tests/unit/models/player-test.js
@@ -0,0 +1,17 @@
+import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+
+moduleForModel('player', 'Unit | Model | player', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('should increment level when told to', function(assert) {
+  // this.subject aliases the createRecord method on the model
+  const player = this.subject({ level: 4 });
+
+  Ember.run(() => player.levelUp());
+
+  assert.equal(player.get('level'), 5, 'level gets incremented');
+  assert.equal(player.get('levelName'), 'Professional', 'new level is called professional');
+});
